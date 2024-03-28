29fce78d34a202c896fcc8b7d3e0573d0019e4b6
adminUbuntu <i.titov@it-students.fr>
2024-03-28 16:34:42 +0100

    init

--- makeUser.sh
+++ makeUser.sh
@@ -1,26 +1,68 @@
-username="$1"
-password="$2"
-password
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Run as root"
+    exit
+fi 
 
 function createUser(){
+    username="$1"
+    echo $username
     echo "Create a new user"
     if id "$username" &>/dev/null; then
-        echo "User '$username' exist"
+        echo "User '$username' exists"
         exit 0
     else    
-        echo "User '$username' doesnt exist"
-        sudo adduser --disabled-password --gecos "" $username
+        adduser --disabled-password --gecos "" "$username"
         echo "User '$username' was added"
     fi
 }
 
 function setPassword(){
-    echo "Add password for user"
-    if [ -z "$password" ];then
-        echo "Password is not correct"
-        exit 0
-    fi
-    echo "$username:$password" | chpasswd
+    username="$1"
+    pwd="$2"
+    echo "Add password for user:" "$username"
+    echo "$username:$pwd" | chpasswd --e
 }
-createUser
-setPassword
+
+function install(){
+    apt update
+}
+
+function installNginx(){
+    apt install nginx
+}
+function tepmpate(){
+    echo "$site"
+}
+function configure_site(){
+    site="$1"
+    port="$2"
+    path="/etc/nginx/sites-available/$site"
+    if [ -e "$path" ];then
+        echo "$site existe"
+        return 1
+    fi
+    
+    cp "/etc/nginx/sites-available/default" "$path"
+
+    sed -i "s/listen .*/listen [::]:$port default_server;/g; s/root \/var\/www\/ .*/var/www/$site;/g; s/server_name .*/server_name $site;/g" "$path"
+    
+    mkdir -p "/var/www/$site"
+
+    echo "$site" > "/var/www/$site/index.html"
+}
+
+case $1 in
+    install | i)
+        install
+        installNginx
+    ;;
+    user | u)
+        createUser "$2"
+        setPassword "$2" "$3"
+    ;;
+    configure_site | cs)
+        configure_site "$2" "$3"
+    ;;
+esac
